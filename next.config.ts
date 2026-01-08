4327dadce30c2146aa06328b27113ee3672941b0
Rafi Ud Deen <RAFIU@Machine.(none)>
2026-01-08 18:01:46 +0530

    Allow external images

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.transparenttextures.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
