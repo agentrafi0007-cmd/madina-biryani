ea06fad78ae3eb4567112af4664d3477b2322af8
Rafi Ud Deen <RAFIU@Machine.(none)>
2026-01-26 11:58:29 +0530

    Add HTTP version to sitemap to resolve redirect issues

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -12,5 +12,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'weekly',
       priority: 1,
     },
+    {
+      url: `http://madinabiryani.site/`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
   ]
 }
